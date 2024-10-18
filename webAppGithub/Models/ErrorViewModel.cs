1ba533c0bf33246a3adb2c48a0fe8796140c651d
anuragM239 <anuragmummadi@yahoo.in>
2024-10-18 15:28:43 +0530

    first commit

---
+++ webAppGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webAppGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
